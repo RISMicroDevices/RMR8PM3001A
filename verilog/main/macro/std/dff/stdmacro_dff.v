709825fd2e1aa29a39903079edac652069e935a9
Kumonda221 <kumonda@kucro3.org>
2021-10-05 17:03:07 +0800

    Revise DFF STDMACRO modules

--- verilog/main/macro/std/dff/stdmacro_dff.v
+++ verilog/main/macro/std/dff/stdmacro_dff.v
@@ -13,7 +13,8 @@
 `include "macro/conf/confmacro_dff.v"
 
 module stdmacro_dff #(
-    parameter   DFF_WIDTH       = 1
+    parameter   DFF_WIDTH           = 1,
+    parameter   DFF_RESET_VALUE     = 'b0
 ) (
     input   wire                        clk,
     input   wire                        reset,
@@ -25,7 +26,8 @@ module stdmacro_dff #(
 `ifdef STDMACRO_DFF_RESET_SYNC
 `ifdef STDMACRO_DFF_RESET_ON_HIGH
     std_dffr #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .reset(reset),
@@ -35,7 +37,8 @@ module stdmacro_dff #(
 `endif
 `ifdef STDMACRO_DFF_RESET_ON_LOW
     std_dffrn #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .resetn(reset),
@@ -48,7 +51,8 @@ module stdmacro_dff #(
 `ifdef STDMACRO_DFF_RESET_ASYNC
 `ifdef STDMACRO_DFF_RESET_ON_HIGH
     std_dffra #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .areset(reset),
@@ -58,7 +62,8 @@ module stdmacro_dff #(
 `endif
 `ifdef STDMACRO_DFF_RESET_ON_LOW
     std_dffran #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .aresetn(reset),
--- verilog/main/macro/std/dff/stdmacro_dffe.v
+++ verilog/main/macro/std/dff/stdmacro_dffe.v
@@ -13,7 +13,8 @@
 `include "macro/conf/confmacro_dff.v"
 
 module stdmacro_dffe #(
-    parameter   DFF_WIDTH       = 1
+    parameter   DFF_WIDTH           = 1,
+    parameter   DFF_RESET_VALUE     = 'b0
 ) (
     input   wire                        clk,
     input   wire                        reset,
@@ -26,7 +27,8 @@ module stdmacro_dffe #(
 `ifdef STDMACRO_DFF_RESET_SYNC
 `ifdef STDMACRO_DFF_RESET_ON_HIGH
     std_dffer #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .reset(reset),
@@ -37,7 +39,8 @@ module stdmacro_dffe #(
 `endif
 `ifdef STDMACRO_DFF_RESET_ON_LOW
     std_dffern #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .resetn(reset),
@@ -51,7 +54,8 @@ module stdmacro_dffe #(
 `ifdef STDMACRO_DFF_RESET_ASYNC
 `ifdef STDMACRO_DFF_RESET_ON_HIGH
     std_dffera #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .areset(reset),
@@ -62,7 +66,8 @@ module stdmacro_dffe #(
 `endif
 `ifdef STDMACRO_DFF_RESET_ON_LOW
     std_dfferan #(
-        .DFF_WIDTH(DFF_WIDTH)
+        .DFF_WIDTH(DFF_WIDTH),
+        .DFF_RESET_VALUE(DFF_RESET_VALUE)
     ) std_macro_dff_INST (
         .clk(clk),
         .aresetn(reset),
