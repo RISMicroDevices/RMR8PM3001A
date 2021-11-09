2970d7bfa288b462e3b3c82838f3f3f24f5e70d2
Kumonda221 <kumonda@kucro3.org>
2021-11-09 13:32:51 +0800

    Commit testbench of RAT Freelist module

---
+++ test/difftest/rat_freelist/csrc/main.cpp
@@ -0,0 +1,146 @@
+#include "verilated.h"
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+//#define DIFF_DEBUG
+
+#ifdef VM_TRACE         // --trace
+#include <verilated_vcd_c.h>
+static VerilatedVcdC* fp;      //to form *.vcd file
+#endif
+
+#include "Vsim_rat_freelist.h"
+static Vsim_rat_freelist* dut_ptr;
+
+void clkp_dumpgen(int& time)
+{
+    dut_ptr->clk = 1;
+
+    dut_ptr->eval();
+
+#ifdef VM_TRACE
+    fp->dump(time++);
+#endif
+}
+
+void clkn_dumpgen(int& time)
+{
+    dut_ptr->clk = 0;
+
+    dut_ptr->eval();
+
+#ifdef VM_TRACE
+    fp->dump(time++);
+#endif
+}
+
+void clk_dumpgen(int &time)
+{
+    clkp_dumpgen(time);
+    clkn_dumpgen(time);
+}
+
+void reset(int& t)
+{
+    dut_ptr->resetn = 0;
+
+    clk_dumpgen(t);
+    clkp_dumpgen(t);
+
+    dut_ptr->resetn = 1;
+
+    printf("[##] \033[1;33mCircuit reset.\033[0m\n");
+}
+
+int testbench_0(int& t)
+{
+    int error = 0;
+
+    // Testbench #0
+    // Verify post-reset state.
+    printf("[#0] Testbench #0\n");
+    printf("[#0] \033[1;33mStarting at clock edge %d (ps)\033[0m\n", t);
+    printf("[#0] Verify on post-reset state.\n");
+
+    // TODO
+
+    //
+    clkn_dumpgen(t);
+    clkp_dumpgen(t);
+
+    //
+    if (error)
+        printf("[#0] Testbench #0 \033[1;31mFAILED\033[0m !!!\n");
+    else
+        printf("[#0] Testbench #0 \033[1;32mPASSED\033[0m !!!\n");
+
+    return error;
+}
+
+void test()
+{
+    srand(time(0));
+
+    int t = 0;
+    int e = 0;
+
+    printf("[--] ----------------------------------------\n");
+
+    printf("[##] Testing on module '\033[1;33missue_rat_freelist\033[0m'\n");
+
+    printf("[--] ----------------------------------------\n");
+
+    reset(t);
+
+    printf("[--] ----------------------------------------\n");
+
+    e += testbench_0(t);
+
+    printf("[--] ----------------------------------------\n");
+
+    printf("Test ");
+
+    if (e)
+        printf("\033[1;31mFAILED\033[0m");
+    else
+        printf("\033[1;32mPASSED\033[0m");
+
+    printf(", %d error(s).\n", e);
+}
+
+int main()
+{
+    dut_ptr = new Vsim_rat_freelist;
+    printf("\033[1;33mRAT Freelist of Issue Stage module 'issue_rat_freelist' selected.\033[0m\n");
+
+#ifdef VM_TRACE
+    ////// !!!  ATTENTION  !!!//////
+    //  Call Verilated::traceEverOn(true) first.
+    //  Then create a VerilatedVcdC object.    
+    Verilated::traceEverOn(true);
+    printf("Enabling waves ...\n");
+    fp = new VerilatedVcdC;     //instantiating .vcd object   
+    dut_ptr->trace(fp, 99);     //trace 99 levels of hierarchy
+    fp->open("vlt_dump.vcd");   
+    fp->dump(0);
+#endif
+
+    // payload
+    test();
+
+    // finalize
+#ifdef VM_TRACE
+    fp->close();
+    delete fp;
+#endif
+    delete dut_ptr;
+
+    printf("\033[1;33mFinalized.\033[0m\n");
+
+    return 0;
+}
---
+++ test/difftest/rat_freelist/vsrc/sim_rat_freelist.v
@@ -0,0 +1,47 @@
+
+//
+
+module sim_rat_freelist (
+    input   wire                clk,
+    input   wire                resetn,
+
+    input   wire [5:0]          i_redeemed_prf,
+    input   wire                i_redeemed_valid,
+    output  wire                o_redeemed_ready,
+
+    output  wire [5:0]          o_acquire_prf,
+    output  wire                o_acquire_valid,
+    input   wire [2:0]          i_acquire_fgr,
+    input   wire                i_acquire_fgr_speculative,
+    input   wire                i_acquire_ready,
+
+    input   wire [2:0]          i_commit_fgr,
+    input   wire                i_commit_valid,
+
+    input   wire [2:0]          i_abandon_fgr,
+    input   wire                i_abandon_valid
+);
+
+    issue_rat_freelist #(
+    ) issue_rat_freelist_INST_sim (
+        .clk                        (clk),
+        .reset                      (resetn),
+
+        .i_redeemed_prf             (i_redeemed_prf),
+        .i_redeemed_valid           (i_redeemed_valid),
+        .o_redeemed_ready           (o_redeemed_ready),
+
+        .o_acquire_prf              (o_acquire_prf),
+        .o_acquire_valid            (o_acquire_valid),
+        .i_acquire_fgr              (i_acquire_fgr),
+        .i_acquire_fgr_speculative  (i_acquire_fgr_speculative),
+        .i_acquire_ready            (i_acquire_ready),
+
+        .i_commit_fgr               (i_commit_fgr),
+        .i_commit_valid             (i_commit_valid),
+
+        .i_abandon_fgr              (i_abandon_fgr),
+        .i_abandon_valid            (i_abandon_valid)
+    );
+
+endmodule
