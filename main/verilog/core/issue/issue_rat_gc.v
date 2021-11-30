4af75c0b4440d48edb9836aec8691af8154b339e
Kumonda221 <kumonda@kucro3.org>
2021-11-30 14:28:18 +0800

    Re-design Register Renaming and Issue Stage

---
+++ main/verilog/core/issue/issue_rat_gc.v
@@ -0,0 +1,17 @@
+// @description
+//  ** PART OF **
+//  RMR8PM3001A - Taurus 3001
+//  (RISC-V 64-bit Privileged Minimal System Processor for T110 ASIC)
+//
+//  Register Alias Table Global Checkpoints
+//
+// @author Kumonda221
+//
+
+module issue_rat_gc #(
+    
+) (
+
+);
+
+endmodule
