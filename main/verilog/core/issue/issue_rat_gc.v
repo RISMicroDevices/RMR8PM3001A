58722530b5f9016d7024a02224be5d5dee069913
Kumonda221 <kumonda@kucro3.org>
2021-12-01 09:54:02 +0800

    Commit RAT Global Checkpoint module

--- main/verilog/core/issue/issue_rat_gc.v
+++ main/verilog/core/issue/issue_rat_gc.v
@@ -9,9 +9,33 @@
 //
 
 module issue_rat_gc #(
-    
+    parameter           GC_WIDTH        = 64,
+    parameter           GC_COUNT_LOG2   = 3
 ) (
+    input   wire                        clk,
+    input   wire                        reset,
 
+    //
+    input   wire [GC_COUNT_LOG2 - 1:0]  addr,
+    input   wire                        we,
+
+    output  wire [GC_WIDTH - 1:0]       dout,
+    input   wire [GC_WIDTH - 1:0]       din
 );
+
+    common_dffram_1a1w1r #(
+        .RAM_DATA_WIDTH (GC_WIDTH),
+        .RAM_ADDR_WIDTH (GC_COUNT_LOG2)
+    ) common_dffram_1a1w1r_INST (
+        .clk    (clk),
+        .reset  (reset),
+
+        .addr   (addr),
+        .en     (1'b1),
+        .we     (we),
+        
+        .dout   (dout),
+        .din    (din)
+    );
 
 endmodule
