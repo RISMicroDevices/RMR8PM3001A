40c592f6e061abc92c927c24c49912a4a2749a48
Kumonda221 <kumonda@kucro3.org>
2021-12-10 01:57:22 +0800

    Revise DFF-base RAM modules

--- main/verilog/common/mem/ram/common_dffram_2a1w1r.v
+++ main/verilog/common/mem/ram/common_dffram_2a1w1r.v
@@ -12,58 +12,131 @@
 `define     RAM_DEPTH       (1 << RAM_ADDR_WIDTH)
 
 module common_dffram_2a1w1r #(
-    parameter                                       RAM_DATA_WIDTH      = 1,
-    parameter                                       RAM_ADDR_WIDTH      = 1,
-    parameter [`RAM_DEPTH * RAM_DATA_WIDTH - 1:0]   RAM_RESET_VALUE     = { (`RAM_DEPTH){ {(RAM_DATA_WIDTH){1'b0}} } }
+    parameter                                       RAM_DATA_WIDTH          = 1,
+    parameter                                       RAM_DEPTH               = 1,
+    parameter [RAM_DEPTH * RAM_DATA_WIDTH - 1:0]    RAM_RESET_VALUE         = { (RAM_DEPTH){ {(RAM_DATA_WIDTH){1'b0}} } },
+
+    parameter                                       PORTA_ONEHOT_ADDRESSING = 0,
+    parameter                                       PORTA_BIT_WRITE_ENABLE  = 0,
+
+    parameter                                       PORTB_ONEHOT_ADDRESSING = 0,
+
+    localparam                                      RAM_ADDR_PORTA_WIDTH    = PORTA_ONEHOT_ADDRESSING ? RAM_DEPTH : $clog2(RAM_DEPTH),
+    localparam                                      RAM_WE_PORTA_WIDTH      = PORTA_BIT_WRITE_ENABLE ? RAM_DATA_WIDTH : 1,
+
+    localparam                                      RAM_ADDR_PORTB_WIDTH    = PORTB_ONEHOT_ADDRESSING ? RAM_DEPTH : $clog2(RAM_DEPTH)
 ) (
-    input  wire                             clk,
-    input  wire                             reset,
+    input  wire                                 clk,
+    input  wire                                 reset,
 
     // Port A - write only
-    input  wire [RAM_ADDR_WIDTH - 1:0]      addra,
-    input  wire                             ena,
-    input  wire                             wea,
+    input  wire [RAM_ADDR_PORTA_WIDTH - 1:0]    addra,
+    input  wire                                 ena,
+    input  wire [RAM_WE_PORTA_WIDTH - 1:0]      wea,
 
-    input  wire [RAM_DATA_WIDTH - 1:0]      dina,
+    input  wire [RAM_DATA_WIDTH - 1:0]          dina,
 
     // Port B - read only
-    input  wire [RAM_ADDR_WIDTH - 1:0]      addrb,
+    input  wire [RAM_ADDR_PORTB_WIDTH - 1:0]    addrb,
 
-    output wire [RAM_DATA_WIDTH - 1:0]      doutb
+    output wire [RAM_DATA_WIDTH - 1:0]          doutb
 );
+    //
+    wire [RAM_DATA_WIDTH * RAM_DEPTH - 1:0]     dff_o_dout;
+    wire [(1 << ($clog2(RAM_DEPTH))) - 1:0]     dff_i_addra;
+    wire [(1 << ($clog2(RAM_DEPTH))) - 1:0]     dff_i_addrb;
 
     //
-    localparam  RAM_DEPTH   = `RAM_DEPTH;
+    generate 
+        if (PORTA_ONEHOT_ADDRESSING) begin :GENERATED_ADDRESS_INPUT_PORTA_ONEHOT
+            
+            assign dff_i_addra = addra;
+        end
+        else begin :GENERATED_ADDRESS_INPUT_PORTA_BINARY
+            
+            macro_decoder_onehot_bin #(
+                .INPUT_WIDTH    (RAM_ADDR_PORTA_WIDTH)
+            ) macro_decoder_onehot_bin_INST_addra (
+                .d  (addra),
+                .q  (dff_i_addra)
+            );
+        end
 
-    wire [RAM_DATA_WIDTH - 1:0]     dff_dout [RAM_DEPTH - 1:0];
+        if (PORTB_ONEHOT_ADDRESSING) begin :GENERATED_ADDRESS_INPUT_PORTB_ONEHOT
 
+            assign dff_i_addrb = addrb;
+        end
+        else begin :GENERATED_ADDRESS_INPUT_PORTB_BINARY
+            
+            macro_decoder_onehot_bin #(
+                .INPUT_WIDTH    (RAM_ADDR_PORTB_WIDTH)
+            ) macro_decoder_onehot_bin_INST_addrb (
+                .d  (addrb),
+                .q  (dff_i_addrb)
+            );
+        end
+    endgenerate
+
+    //
     genvar i;
     generate
         for (i = 0; i < RAM_DEPTH; i = i + 1) begin :GENERATED_RAM_DFFS
 
             //
-            wire    dff_we;
+            wire [RAM_DATA_WIDTH - 1:0]     dff_q;
+            wire [RAM_WE_PORTA_WIDTH - 1:0] dff_we;
 
-            stdmacro_dffe #(
-                .DFF_WIDTH          (RAM_DATA_WIDTH),
-                .DFF_RESET_VALUE    (RAM_RESET_VALUE[RAM_DATA_WIDTH * i +: RAM_DATA_WIDTH])
-            ) stdmacro_dffe_INST_dffram_dff (
-                .clk    (clk),
-                .reset  (reset),
+            //
+            if (PORTA_BIT_WRITE_ENABLE) begin
 
-                .en     (dff_we),
-                .d      (dina),
+                stdmacro_dffbe #(
+                    .DFF_WIDTH          (RAM_DATA_WIDTH),
+                    .DFF_RESET_VALUE    (RAM_RESET_VALUE[RAM_DATA_WIDTH * i +: RAM_DATA_WIDTH])
+                ) stdmacro_dffbe_INST_dffram_dff (
+                    .clk    (clk),
+                    .reset  (reset),
 
-                .q      (dff_dout[i])
-            );
+                    .en     (dff_we),
+                    .d      (dina),
 
-            assign dff_we  = wea & ena & (addra == i);
+                    .q      (dff_q)
+                );
+
+                assign dff_we  = wea & { (RAM_DATA_WIDTH){ena} } & { (RAM_DATA_WIDTH){ dff_i_addra[i] } };
+            end
+            else begin
+
+                stdmacro_dffe #(
+                    .DFF_WIDTH          (RAM_DATA_WIDTH),
+                    .DFF_RESET_VALUE    (RAM_RESET_VALUE[RAM_DATA_WIDTH * i +: RAM_DATA_WIDTH])
+                ) stdmacro_dffe_INST_dffram_dff (
+                    .clk    (clk),
+                    .reset  (reset),
+
+                    .en     (dff_we),
+                    .d      (dina),
+
+                    .q      (dff_q)
+                );
+
+                assign dff_we  = wea & ena & dff_i_addra[i];
+            end
+
+            //
+            assign dff_o_dout[RAM_DATA_WIDTH * i +: RAM_DATA_WIDTH] = dff_q & {(RAM_DATA_WIDTH){ dff_i_addrb[i] }};
+
             //
         end
     endgenerate
 
     //
-    assign  doutb = dff_dout[addrb];
+    macro_reduction_or #(
+        .INPUT_WIDTH    (RAM_DATA_WIDTH),
+        .INPUT_COUNT    (RAM_DEPTH)
+    ) macro_reduction_or_INST_output (
+        .d  (dff_o_dout),
+        .q  (doutb)
+    );
 
     //
 
