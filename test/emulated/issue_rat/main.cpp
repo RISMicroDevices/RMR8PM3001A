39a5e9b6b39ce46e501c0eab4486360a3cd1682e
Kumonda221 <kumonda@kucro3.org>
2022-01-21 23:31:59 +0800

    Commit MEMU verification tree

---
+++ test/emulated/issue_rat/main.cpp
@@ -0,0 +1,15 @@
+
+// Verification module (console) for Issue Stage Register Alias Table
+
+#include <iostream>
+
+#include "core_issue.hpp"
+
+using namespace std;
+
+int main()
+{
+    // TODO
+
+    return 0;
+}
