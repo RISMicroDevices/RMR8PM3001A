417e8a7f7d29512660ebc18779f9ea1fa788d337
Kumonda221 <kumonda@kucro3.org>
2022-01-24 17:35:10 +0800

    Commit testbench of Issue Stage RAT with VMC

--- test/emulated/issue_rat/vmc_rat.hpp
+++ test/emulated/issue_rat/vmc_rat.hpp
@@ -1,22 +1,38 @@
 #pragma once
 
+#include <list>
+
 #include "core_global.hpp"
 
 
 namespace VMC::RAT {
 
+    static constexpr int INSN_NOP   = 0;
+
     class SimInstruction
     {
     private:
-        int     dstARF;
         int     clkDelay;
+        int     dstARF;
+        int     srcARF1;
+        int     srcARF2;
+        int     insncode;
 
     public:
-        SimInstruction(int dstARF, int clkDelay);
+        SimInstruction();
+        SimInstruction(int clkDelay, int dstARF);
+        SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2);
+        SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2, int insncode);
+        SimInstruction(const SimInstruction& obj);
         ~SimInstruction();
 
         int     GetRemainingClkDelay() const;
         int     GetDstARF() const;
+        int     GetSrcARF1() const;
+        int     GetSrcARF2() const;
+
+        void    SetInsnCode(int insncode);
+        int     GetInsnCode() const;
 
         bool    Immediate() const;
 
@@ -25,7 +41,30 @@ namespace VMC::RAT {
 
     class SimReOrderBuffer
     {
+    private:
+        list<SimInstruction>    insnBuffer;
+        SimInstruction          insnReady;
+        bool                    insnReadyFlag;
+        SimInstruction          insnPush;
+        bool                    insnPushFlag;
+        bool                    insnPopFlag;
 
+    public:
+        SimReOrderBuffer();
+        ~SimReOrderBuffer();
+
+        bool            IsNextReady() const;
+        SimInstruction  GetNextReady() const;
+
+        int             GetInsnCount() const;
+        void            Clear();
+
+        void            PushInsn(SimInstruction insn);
+        void            PopInsn();
+
+        void            ResetInput();
+
+        void            Eval();
     };
 
     class SimHandle 
@@ -49,9 +88,44 @@ namespace VMC::RAT {
     int     clkDelay;
     */
 
-    SimInstruction::SimInstruction(int dstARF, int clkDelay)
-        : dstARF    (dstARF)
-        , clkDelay  (clkDelay)
+    SimInstruction::SimInstruction()
+        : clkDelay  (0)
+        , dstARF    (0)
+        , srcARF1   (0)
+        , srcARF2   (0)
+        , insncode  (INSN_NOP)
+    { }
+
+    SimInstruction::SimInstruction(int clkDelay, int dstARF)
+        : clkDelay  (clkDelay)
+        , dstARF    (dstARF)
+        , srcARF1   (0)
+        , srcARF2   (0)
+        , insncode  (INSN_NOP)
+    { }
+
+    SimInstruction::SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2)
+        : clkDelay  (clkDelay)
+        , dstARF    (dstARF)
+        , srcARF1   (srcARF1)
+        , srcARF2   (srcARF2)
+        , insncode  (INSN_NOP)
+    { }
+
+    SimInstruction::SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2, int insncode)
+        : clkDelay  (clkDelay)
+        , dstARF    (dstARF)
+        , srcARF1   (srcARF1)
+        , srcARF2   (srcARF2)
+        , insncode  (INSN_NOP)
+    { }
+
+    SimInstruction::SimInstruction(const SimInstruction& obj)
+        : clkDelay  (obj.clkDelay)
+        , dstARF    (obj.dstARF)
+        , srcARF1   (obj.srcARF1)
+        , srcARF2   (obj.srcARF2)
+        , insncode  (obj.insncode)
     { }
 
     SimInstruction::~SimInstruction()
@@ -67,6 +141,26 @@ namespace VMC::RAT {
         return dstARF;
     }
 
+    inline int SimInstruction::GetSrcARF1() const
+    {
+        return srcARF1;
+    }
+
+    inline int SimInstruction::GetSrcARF2() const
+    {
+        return srcARF2;
+    }
+
+    inline void SimInstruction::SetInsnCode(int insncode)
+    {
+        this->insncode = insncode;
+    }
+
+    inline int SimInstruction::GetInsnCode() const
+    {
+        return insncode;
+    }
+
     inline bool SimInstruction::Immediate() const
     {
         return clkDelay == 0;
@@ -79,3 +173,70 @@ namespace VMC::RAT {
     }
 }
 
+
+// class VMC::RAT::SimReOrderBuffer
+namespace VMC::RAT {
+    /*
+    list<SimInstruction>    insnBuffer;
+    SimInstruction          insnReady;
+    bool                    insnReadyFlag;
+    SimInstruction          insnPush;
+    bool                    insnPushFlag;
+    bool                    insnPopFlag;
+    */
+
+    SimReOrderBuffer::SimReOrderBuffer()
+        : insnBuffer    (list<SimInstruction>())
+        , insnReady     (SimInstruction())
+        , insnReadyFlag (false)
+        , insnPush      (SimInstruction())
+        , insnPushFlag  (false)
+        , insnPopFlag   (false)
+    { }
+
+    SimReOrderBuffer::~SimReOrderBuffer()
+    { }
+
+    inline bool SimReOrderBuffer::IsNextReady() const
+    {
+        return insnReadyFlag;
+    }
+
+    inline SimInstruction SimReOrderBuffer::GetNextReady() const
+    {
+        return insnReady;
+    }
+
+    inline int SimReOrderBuffer::GetInsnCount() const
+    {
+        return insnBuffer.size();
+    }
+
+    inline void SimReOrderBuffer::Clear()
+    {
+        insnBuffer.clear();
+    }
+
+    void SimReOrderBuffer::PushInsn(SimInstruction insn)
+    {
+        insnPush     = insn;
+        insnPushFlag = true;
+    }
+
+    void SimReOrderBuffer::PopInsn()
+    {
+        insnPopFlag = true;
+    }
+
+    void SimReOrderBuffer::ResetInput()
+    {
+        insnPushFlag = false;
+        insnPopFlag  = false;
+    }
+
+    void SimReOrderBuffer::Eval()
+    {
+        // TODO
+        
+    }
+}
