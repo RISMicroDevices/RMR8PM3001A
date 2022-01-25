84404c275b7838492d37b59380c04213240d2118
Kumonda221 <kumonda@kucro3.org>
2022-01-25 22:57:16 +0800

    Revise VMC basic commands

--- main/vmc/vmc.hpp
+++ main/vmc/vmc.hpp
@@ -102,5 +102,51 @@ namespace VMC {
 
         return true;
     }
+
+}
+
+namespace VMC::Basic {
+
+#define ECHO_COUT_VMC_BASIC_HELP \
+    std::cout << "Basic command usages:" << std::endl; \
+    std::cout << "- version                           Display version of VMC console" << std::endl; \
+    std::cout << "- exit                              Exit VMC console"               << std::endl; \
+    std::cout << "- echo <content...>                 Display information on console" << std::endl;
+
+    //
+    bool Version(void* handle, const std::string& cmd, 
+                               const std::string& paramline, 
+                               const std::vector<std::string>& params)
+    {
+        ECHO_COUT_VMC_VERSION
+        return true;
+    }
+
+    //
+    bool Exit(void* handle, const std::string& cmd, 
+                            const std::string& paramline, 
+                            const std::vector<std::string>& params)
+    {
+        exit(EXIT_SUCCESS);
+        return true;
+    }
+
+    //
+    bool Echo(void* handle, const std::string& cmd, 
+                            const std::string& paramline, 
+                            const std::vector<std::string>& params)
+    {
+        std::cout << paramline << std::endl;
+        return true;
+    }
+
+
+    //
+    void SetupCommands(VMCHandle handle)
+    {
+        handle->handlers.push_back(VMC::CommandHandler { std::string("version"), &Version });
+        handle->handlers.push_back(VMC::CommandHandler { std::string("exit")   , &Exit });
+        handle->handlers.push_back(VMC::CommandHandler { std::string("echo")   , &Echo }); 
+    }
     
 }
