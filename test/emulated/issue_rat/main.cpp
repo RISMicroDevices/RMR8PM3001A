c99d1fa5ecfc6725071a25896e82bb6da3ab54c7
Kumonda221 <kumonda@kucro3.org>
2022-01-25 22:55:36 +0800

    Commit testbench of Issue Stage RAT with VMC

--- test/emulated/issue_rat/main.cpp
+++ test/emulated/issue_rat/main.cpp
@@ -14,54 +14,32 @@
 
 VMC::VMCEntity con;
 
-bool Version(void* handle, const std::string& cmd, 
-                           const std::string& paramline, 
-                           const std::vector<std::string>& params)
-{
-    ECHO_COUT_VMC_VERSION
-    return true;
-}
-
 bool Help(void* handle, const std::string& cmd,
                         const std::string& paramline,
                         const std::vector<std::string>& params)
 {
     std::cout << "Command usages: " << std::endl;
-    std::cout << "- help                          Display help information" << std::endl;
-    std::cout << "- version                       Display version of VMC console" << std::endl;
-    std::cout << "- exit                          Exit VMC console" << std::endl;
-    std::cout << "- echo <content...>             Display information on console" << std::endl;
+    std::cout << "- help                              Display help information" << std::endl;
+    ECHO_COUT_VMC_BASIC_HELP
+    ECHO_COUT_VMC_RAT_HELP
     return true;
 }
 
-bool Exit(void* handle, const std::string& cmd, 
-                        const std::string& paramline, 
-                        const std::vector<std::string>& params)
-{
-    exit(EXIT_SUCCESS);
-    return true;
-}
 
-bool Echo(void* handle, const std::string& cmd, 
-                        const std::string& paramline, 
-                        const std::vector<std::string>& params)
-{
-    std::cout << paramline << std::endl;
-    return true;
-}
 
 void InitBasicCommands(VMC::VMCHandle handle)
 {
     handle->handlers.push_back(VMC::CommandHandler { std::string("?")      , &Help });
     handle->handlers.push_back(VMC::CommandHandler { std::string("help")   , &Help });
-    handle->handlers.push_back(VMC::CommandHandler { std::string("version"), &Version });
-    handle->handlers.push_back(VMC::CommandHandler { std::string("exit")   , &Exit });
-    handle->handlers.push_back(VMC::CommandHandler { std::string("echo")   , &Echo }); 
+
+    VMC::Basic::SetupCommands(handle);
 }
 
 int main()
 {
     InitBasicCommands(&con);
+
+    VMC::RAT::Setup(&con);
 
     ECHO_COUT_VMC_VERSION
     std::cout << std::endl;
--- test/emulated/issue_rat/vmc_rat.hpp
+++ test/emulated/issue_rat/vmc_rat.hpp
@@ -1,13 +1,32 @@
 #pragma once
 
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <list>
+#include <vector>
 
-#include "core_global.hpp"
+#include "vmc.hpp"
+
+#include "core_issue.hpp"
 
 
 namespace VMC::RAT {
 
-    static constexpr int INSN_NOP   = 0;
+    static constexpr int INSN_CODE_NOP      = 0;
+
+    static constexpr int INSN_CODE_AND      = 1;
+
+    static constexpr int INSN_CODE_OR       = 2;
+
+    static constexpr int INSN_CODE_XOR      = 3;
+
+    static constexpr int INSN_CODE_ADD      = 4;
+
+    static constexpr int INSN_CODE_SUB      = 5;
+
+    static constexpr int INSN_COUNT         = 6;
 
     class SimInstruction
     {
@@ -57,6 +76,7 @@ namespace VMC::RAT {
         SimInstruction  GetNextReady() const;
 
         int             GetInsnCount() const;
+        bool            IsEmpty() const;
         void            Clear();
 
         void            PushInsn(SimInstruction insn);
@@ -67,18 +87,216 @@ namespace VMC::RAT {
         void            Eval();
     };
 
-    class SimHandle 
-    {
+    typedef struct {
 
-    };
+        SimReOrderBuffer    ROB                         = SimReOrderBuffer();
+
+        int                 PRF[EMULATED_PRF_SIZE]      = { 0 };
+
+        int                 RefARF[EMULATED_ARF_SIZE]   = { 0 };
+
+        bool                FlagStepInfo                = false;
+
+        bool                FlagARF0Conv                = true;
+
+    } SimContext, *SimHandle;
 }
 
 
 // Global components
 namespace VMC::RAT {
 
+    static SimHandle CURRENT_HANDLE = nullptr;
+
+    void SetupCommands(VMCHandle handle);
+
+    inline SimHandle NewHandle()
+    {
+        return new SimContext;
+    }
+
+    inline void DeleteHandle(SimHandle handle)
+    {
+        delete handle;
+    }
+
+    void Setup(VMCHandle handle)
+    {
+        CURRENT_HANDLE = NewHandle();
+        
+        SetupCommands(handle);
+    }
     
-};
+    inline void SetCurrentHandle(SimHandle handle)
+    {
+        CURRENT_HANDLE = handle;
+    }
+
+    inline SimHandle GetCurrentHandle()
+    {
+        return CURRENT_HANDLE;
+    }
+
+    //
+}
+
+
+// VMC RAT commands
+namespace VMC::RAT {
+
+#define ECHO_COUT_VMC_RAT_HELP \
+    std::cout << "RAT0 simulation command usages:" << std::endl; \
+    std::cout << "- rat0.info.bystep [true|false]     Toggle by-step info of RAT0 simulation (\'false\' by default)" << std::endl; \
+    std::cout << "- rat0.arf0conv [true|false]        Toggle ARF0-zero conversion (\'true\' by default)" << std::endl; \
+    std::cout << "- rat0.rat.ls [-V|+V|-NRA|+NRA|-FV|+FV] " << std::endl; \
+    std::cout << "                                    List all RAT entries (with optional filter)" << std::endl; \
+    std::cout << "- rat0.arf.ls.ref [-Z]              List all reference ARF register values (with optional filter)" << std::endl; \
+    std::cout << "- rat0.arf.ls [-Z|-U]               List all values of ARF register mapped by RAT (with optional filter)" << std::endl; \
+    std::cout << "- rat0.arf.set <index> <value>      Set specified ARF register value" << std::endl; \
+    std::cout << "- rat0.arf.set.random [index]       Set specified/random ARF register with random value" << std::endl; \
+    std::cout << "- rat0.arf.get <index>              Get specified ARF register value" << std::endl;
+
+    // rat0.info.bystep [true|false]
+    bool _RAT0_INFO_BYSTEP(void* handle, const std::string& cmd,
+                                         const std::string& paramline,
+                                         const std::vector<std::string>& params)
+    {
+        if (params.size() > 1)
+        {
+            std::cout << "Too much or too less parameter(s) for \'rat0.info.bystep\'." << std::endl;
+            return false;
+        }
+
+        if (!params.empty())
+        {
+            std::istringstream(params[0]) >> std::boolalpha >> GetCurrentHandle()->FlagStepInfo;
+            std::cout << "Set: ";
+        }
+
+        std::cout << "RAT0.flag.stepinfo = ";
+        std::cout << GetCurrentHandle()->FlagStepInfo << std::endl;
+
+        return true;
+    }
+
+    // rat0.arf0conv [true|false]
+    bool _RAT0_ARF0CONV(void* handle, const std::string& cmd,
+                                      const std::string& paramline,
+                                      const std::vector<std::string>& params)
+    {
+        if (params.size() > 1)
+        {
+            std::cout << "Too much or too less parameter(s) for \'rat0.arf0conv\'." << std::endl;
+            return false;
+        }
+
+        if (!params.empty())
+        {
+            std::istringstream(params[0]) >> std::boolalpha >> GetCurrentHandle()->FlagARF0Conv;
+            std::cout << "Set: ";
+        }
+
+        std::cout << "RAT0.flag.arf0conv = ";
+        std::cout << GetCurrentHandle()->FlagARF0Conv << std::endl;
+
+        return true;
+    }
+
+    // rat0.arf.ls.ref [-Z]
+    bool _RAT0_ARF_LS_REF(void* handle, const std::string& cmd,
+                                        const std::string& paramline,
+                                        const std::vector<std::string>& params)
+    {
+        //
+        bool nonzero = false;
+
+        if (params.size() == 1)
+        {
+            if (params[0].compare("-Z") == 0)
+            {
+                nonzero = true;
+            }
+            else
+            {
+                std::cout << "Invalid parameter: \'" << params[0] << "\'" << std::endl;
+                return false;
+            }
+        }
+        else if (!params.empty())
+        {
+            std::cout << "Too much or too less parameter(s) for \'rat0.arf.ls.ref\'." << std::endl;
+            return false;
+        }
+
+        //
+        std::cout << "Architectural registers:" << std::endl;
+
+        if (nonzero)
+            std::cout << "(\'-Z\': Listing all non-zero registers)" << std::endl;
+
+        std::cout << "Type         Index         Value" << std::endl;
+        std::cout << "-----        ------        -----------" << std::endl;
+
+        for (int i = 0; i < EMULATED_ARF_SIZE; i++)
+        {
+            if (nonzero)
+                if (!GetCurrentHandle()->RefARF[i])
+                    continue;
+
+            std::cout << "rARF         ";
+            printf("%3d", i);
+            std::cout << "           ";
+            printf("0x%08x\n", GetCurrentHandle()->RefARF[i]);
+        }
+
+        return true;
+    }
+
+    
+    // rat0.arf.ls [-Z|-U]
+    bool _RAT0_ARF_LS(void* handle, const std::string& cmd,
+                                    const std::string& paramline,
+                                    const std::vector<std::string>& params)
+    {
+        // TODO
+
+        return true;
+    }
+
+
+    // rat0.arf.set <index> <value>
+
+    
+    // rat0.arf.set.random <index>
+
+    
+    // rat0.arf.get <index>
+
+
+    // rat0.rat.ls [-V|+V|-NRA|+NRA|-FV|+FV]
+    bool _RAT0_RAT_LS(void* handle, const std::string& cmd,
+                                    const std::string& paramline,
+                                    const std::vector<std::string>& params)
+    {
+        // TODO
+
+        return true;
+    }
+
+    // rat0.diffsim.insn <insncode> [delay] [dstARF] [srcARF1] [srcARF2]
+
+
+    // rat0.diffsim.insn.random <count>
+
+
+    void SetupCommands(VMCHandle handle)
+    {
+        handle->handlers.push_back(CommandHandler{ std::string("rat0.info.bystep"), &_RAT0_INFO_BYSTEP });
+        handle->handlers.push_back(CommandHandler{ std::string("rat0.arf0conv")   , &_RAT0_ARF0CONV });
+        handle->handlers.push_back(CommandHandler{ std::string("rat0.rat.ls")     , &_RAT0_RAT_LS });
+        handle->handlers.push_back(CommandHandler{ std::string("rat0.arf.ls.ref") , &_RAT0_ARF_LS_REF });
+    }
+}
 
 
 // class VMC::RAT::SimInstruction
@@ -93,7 +311,7 @@ namespace VMC::RAT {
         , dstARF    (0)
         , srcARF1   (0)
         , srcARF2   (0)
-        , insncode  (INSN_NOP)
+        , insncode  (INSN_CODE_NOP)
     { }
 
     SimInstruction::SimInstruction(int clkDelay, int dstARF)
@@ -101,7 +319,7 @@ namespace VMC::RAT {
         , dstARF    (dstARF)
         , srcARF1   (0)
         , srcARF2   (0)
-        , insncode  (INSN_NOP)
+        , insncode  (INSN_CODE_NOP)
     { }
 
     SimInstruction::SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2)
@@ -109,7 +327,7 @@ namespace VMC::RAT {
         , dstARF    (dstARF)
         , srcARF1   (srcARF1)
         , srcARF2   (srcARF2)
-        , insncode  (INSN_NOP)
+        , insncode  (INSN_CODE_NOP)
     { }
 
     SimInstruction::SimInstruction(int clkDelay, int dstARF, int srcARF1, int srcARF2, int insncode)
@@ -117,7 +335,7 @@ namespace VMC::RAT {
         , dstARF    (dstARF)
         , srcARF1   (srcARF1)
         , srcARF2   (srcARF2)
-        , insncode  (INSN_NOP)
+        , insncode  (INSN_CODE_NOP)
     { }
 
     SimInstruction::SimInstruction(const SimInstruction& obj)
@@ -212,6 +430,11 @@ namespace VMC::RAT {
         return insnBuffer.size();
     }
 
+    inline bool SimReOrderBuffer::IsEmpty() const
+    {
+        return !GetInsnCount();
+    }
+
     inline void SimReOrderBuffer::Clear()
     {
         insnBuffer.clear();
@@ -236,7 +459,40 @@ namespace VMC::RAT {
 
     void SimReOrderBuffer::Eval()
     {
-        // TODO
-        
+        // eval all
+        list<SimInstruction>::iterator iter = insnBuffer.begin();
+        while (iter != insnBuffer.end())
+            (*iter++).Eval();
+
+        // push insn
+        if (insnPushFlag)
+        {
+            insnBuffer.push_back(insnPush);
+            insnPushFlag = false;
+        }
+
+        // pop insn
+        if (insnPopFlag && !insnBuffer.empty())
+        {
+            insnBuffer.pop_front();
+            insnPopFlag = false;
+        }
+
+        // check next ready insn
+        insnReadyFlag = false;
+
+        iter = insnBuffer.begin();
+        while (iter != insnBuffer.end())
+        {
+            if ((*iter).Immediate())
+            {
+                insnReady     = *iter;
+                insnReadyFlag = true;
+
+                break;
+            }
+            else
+                iter++;
+        }
     }
 }
