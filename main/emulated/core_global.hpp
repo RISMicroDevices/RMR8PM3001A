3e44237a98fcfd6eb3bb66f42aa463fbd4a2bd08
Kumonda221 <kumonda@kucro3.org>
2022-03-10 23:29:47 +0800

    Commit mixed emulation of Global Checkpoint Table

--- main/emulated/core_global.hpp
+++ main/emulated/core_global.hpp
@@ -7,16 +7,8 @@
 #include <bitset>
 
 #include "base.hpp"
-
-
-#define EMULATED_GC_COUNT                           8
-#define EMULATED_ARF_SIZE                           32
-#define EMULATED_PRF_SIZE                           96
-
-#define EMULATED_SCOREBOARD_GC_COUNT                EMULATED_GC_COUNT
-
-#define EMULATED_RAT_SIZE                           EMULATED_PRF_SIZE
-#define EMULATED_RAT_GC_COUNT                       EMULATED_GC_COUNT 
+#include "common.hpp"
+#include "core_globaldef.hpp"
 
 
 using namespace std;
@@ -107,30 +99,47 @@ namespace MEMU::Core {
         void                operator=(const CompressingMemory<TPayload>& obj) = delete;
     };
 
-    class GlobalCheckpointTable : public MEMU::Emulated
+
+
+    class GlobalCheckpointTableEntry {
+    private:
+        fgr_t   FGR;
+        bool    V;
+        int     GCA;
+
+    public:
+        GlobalCheckpointTableEntry();
+        GlobalCheckpointTableEntry(const GlobalCheckpointTableEntry& obj);
+        ~GlobalCheckpointTableEntry();
+
+        fgr_t   GetFGR() const;
+        bool    GetValid() const;
+        int     GetGCA() const;
+
+        void    SetFGR(fgr_t FGR);
+        void    SetValid(bool V);
+        void    SetGCA(int GCA);
+
+        void    Clear();
+    };
+
+    class GlobalCheckpointWALK
+    {
+        // TODO
+    };
+
+    class GlobalCheckpointTable 
+        : public MEMU::Common::FIFO<GlobalCheckpointTableEntry>
     {
     private:
-        bitset<EMULATED_GC_COUNT>   gc_valid;
-        int                         gc_next;
-
-        int                         gc_eval_state;
-        int                         gc_eval_index;
+        static constexpr int    size = EMULATED_GC_COUNT;
 
     public:
         GlobalCheckpointTable();
         GlobalCheckpointTable(const GlobalCheckpointTable& obj);
         ~GlobalCheckpointTable();
 
-        constexpr int   GetSize() const;
-
-        bool            IsValid(int index) const;
         
-        int             GetNext() const;
-        int             ValidateNext();
-        void            Invalidate(int index);
-        void            ResetInput();
-
-        virtual void    Eval() override;
     };
 }
 
@@ -434,104 +443,4 @@ namespace MEMU::Core {
 }
 
 
-// class MEMU::Core::GlobalCheckpointTable
-namespace MEMU::Core {
-    /*
-    bitset<EMULATED_GC_COUNT>   gc_valid;
-    int                         gc_next;
-
-    int                         gc_eval_state;
-    int                         gc_eval_index;
-    */
-
-#define __MEMU_CORE_GCT_EVAL_STATE_NOP                  0
-#define __MEMU_CORE_GCT_EVAL_STATE_VALIDATE             1
-#define __MEMU_CORE_GCT_EVAL_STATE_INVALIDATE           2
-
-    GlobalCheckpointTable::GlobalCheckpointTable()
-        : gc_next       (0)
-        , gc_eval_state (__MEMU_CORE_GCT_EVAL_STATE_NOP)
-        , gc_eval_index (-1)
-    { }
-
-    GlobalCheckpointTable::GlobalCheckpointTable(const GlobalCheckpointTable& obj)
-        : gc_valid      (obj.gc_valid)
-        , gc_next       (obj.gc_next)
-        , gc_eval_state (obj.gc_eval_state)
-        , gc_eval_index (obj.gc_eval_index)
-    { }
-
-    GlobalCheckpointTable::~GlobalCheckpointTable()
-    { }
-
-    constexpr int GlobalCheckpointTable::GetSize() const
-    {
-        return EMULATED_GC_COUNT;
-    }
-
-    inline bool GlobalCheckpointTable::IsValid(int index) const
-    {
-        return gc_valid[index];
-    }
-
-    inline int GlobalCheckpointTable::GetNext() const
-    {
-        return gc_next;
-    }
-
-    inline int GlobalCheckpointTable::ValidateNext()
-    {
-        gc_eval_state = __MEMU_CORE_GCT_EVAL_STATE_VALIDATE;
-        gc_eval_index = gc_next;
-
-        return gc_next;
-    }
-
-    inline void GlobalCheckpointTable::Invalidate(int index)
-    {
-        gc_eval_state = __MEMU_CORE_GCT_EVAL_STATE_INVALIDATE;
-        gc_eval_index = index;
-    }
-
-    inline void GlobalCheckpointTable::ResetInput()
-    {
-        gc_eval_state = __MEMU_CORE_GCT_EVAL_STATE_NOP;
-        gc_eval_index = -1;
-    }
-
-    void GlobalCheckpointTable::Eval()
-    {
-        //
-        switch (gc_eval_state)
-        {
-            case __MEMU_CORE_GCT_EVAL_STATE_VALIDATE:
-                gc_valid[gc_eval_index] = true;
-                break;
-
-            case __MEMU_CORE_GCT_EVAL_STATE_INVALIDATE:
-                for (int i = gc_eval_index; i < GetSize(); i++)
-                    gc_valid[i] = false;
-                break;
-
-            case __MEMU_CORE_GCT_EVAL_STATE_NOP:
-            default:
-                break;
-
-        }
-
-        //
-        gc_next = -1;
-
-        for (int i = 0; i < GetSize(); i++)
-            if (!gc_valid[i])
-            {
-                gc_next = i;
-                break;
-            }
-
-        //
-        gc_eval_state = __MEMU_CORE_GCT_EVAL_STATE_NOP;
-        gc_eval_index = -1;
-    }
-}
-
+//
