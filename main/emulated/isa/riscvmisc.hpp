54dbc735472a02c86890d287e9b204d3555682bb
Kumonda221 <kumonda@kucro3.org>
2022-04-14 23:06:58 +0800

    Commit RISC-V ISA encoder utilities

--- main/emulated/isa/riscvmisc.hpp
+++ main/emulated/isa/riscvmisc.hpp
@@ -12,6 +12,29 @@
     ((val & (name##_MASK >> name##_OFFSET)) << name##_OFFSET)
 
 
+#define ENCODE_IMM_RVTYPE_I(imm) \
+    ( (imm & 0x00000FFFU) << 20)
+
+#define ENCODE_IMM_RVTYPE_S(imm) \
+    ( ((imm & 0x0000001FU) << 7) \
+    | ((imm & 0x00000FE0U) << 20)) 
+
+#define ENCODE_IMM_RVTYPE_B(imm) \
+    ( ((imm & 0x00000800U) >> 4) \
+    | ((imm & 0x0000001EU) << 7) \
+    | ((imm & 0x000007E0U) << 20) \
+    | ((imm & 0x00001000U) << 19))
+
+#define ENCODE_IMM_RVTYPE_U(imm) \
+       (imm & 0xFFFFF000U)
+
+#define ENCODE_IMM_RVTYPE_J(imm) \
+    (  (imm & 0x000FF000U) \
+    | ((imm & 0x00000800U) << 9) \
+    | ((imm & 0x000007FEU) << 20) \
+    | ((imm & 0x00100000U) << 11))
+
+
 #define ENCODE_RVTYPE_R(opcode, funct3, rd, rs1, rs2, funct7) \
     ( ENCODE_STD_OPERAND(opcode , RV_OPCODE) \
     | ENCODE_STD_OPERAND(funct3 , RV_FUNCT3) \
@@ -25,38 +48,31 @@
     | ENCODE_STD_OPERAND(funct3 , RV_FUNCT3) \
     | ENCODE_STD_OPERAND(rd     , RV_OPERAND_RD) \
     | ENCODE_STD_OPERAND(rs1    , RV_OPERAND_RS1) \
-    | ((imm & 0x00000FFFU) << 20))
+    | ENCODE_IMM_RVTYPE_I(imm))
 
 #define ENCODE_RVTYPE_S(opcode, funct3, rs1, rs2, imm) \
     ( ENCODE_STD_OPERAND(opcode , RV_OPCODE) \
     | ENCODE_STD_OPERAND(funct3 , RV_FUNCT3) \
     | ENCODE_STD_OPERAND(rs1    , RV_OPERAND_RS1) \
     | ENCODE_STD_OPERAND(rs2    , RV_OPERAND_RS2) \
-    | ((imm & 0x0000001FU) << 7) \
-    | ((imm & 0x00000FE0U) << 20))
+    | ENCODE_IMM_RVTYPE_S(imm))
 
 #define ENCODE_RVTYPE_B(opcode, funct3, rs1, rs2, imm) \
     ( ENCODE_STD_OPERAND(opcode , RV_OPCODE) \
     | ENCODE_STD_OPERAND(funct3 , RV_FUNCT3) \
     | ENCODE_STD_OPERAND(rs1    , RV_OPERAND_RS1) \
     | ENCODE_STD_OPERAND(rs2    , RV_OPERAND_RS2) \
-    | ((imm & 0x00000800U) >> 4) \
-    | ((imm & 0x0000001EU) << 7) \
-    | ((imm & 0x000007E0U) << 20) \
-    | ((imm & 0x00001000U) << 19))
+    | ENCODE_IMM_RVTYPE_B(imm))
 
 #define ENCODE_RVTYPE_U(opcode, rd, imm) \
     ( ENCODE_STD_OPERAND(opcode , RV_OPCODE) \
     | ENCODE_STD_OPERAND(rd     , RV_OPERAND_RD) \
-    | (imm & 0xFFFFF000U))
+    | ENCODE_IMM_RVTYPE_U(imm))
 
 #define ENCODE_RVTYPE_J(opcode, rd, imm) \
     ( ENCODE_STD_OPERAND(opcode , RV_OPCODE) \
     | ENCODE_STD_OPERAND(rd     , RV_OPERAND_RD) \
-    |  (imm & 0x000FF000U) \
-    | ((imm & 0x00000800U) << 9) \
-    | ((imm & 0x000007FEU) << 20) \
-    | ((imm & 0x00100000U) << 11))
+    | ENCODE_IMM_RVTYPE_J(imm))
 
 
 
@@ -87,6 +103,109 @@ namespace Jasse {
     void DecodeNormalRVTypeB(insnraw_t insnraw, RVInstruction& insn) noexcept;
     void DecodeNormalRVTypeU(insnraw_t insnraw, RVInstruction& insn) noexcept;
     void DecodeNormalRVTypeJ(insnraw_t insnraw, RVInstruction& insn) noexcept;
+
+    // RISC-V Normal Instruction Form Encoder
+    class RVEncoderTypeR;
+    class RVEncoderTypeI;
+    class RVEncoderTypeS;
+    class RVEncoderTypeB;
+    class RVEncoderTypeU;
+    class RVEncoderTypeJ;
+    class RVEncoderTypeIZeroOperand;
+
+    // RISC-V Normal Instruction Form Encoder Allocator
+    RVEncoder* AllocRVEncoderTypeR() noexcept;
+    RVEncoder* AllocRVEncoderTypeI() noexcept;
+    RVEncoder* AllocRVEncoderTypeS() noexcept;
+    RVEncoder* AllocRVEncoderTypeB() noexcept;
+    RVEncoder* AllocRVEncoderTypeU() noexcept;
+    RVEncoder* AllocRVEncoderTypeJ() noexcept;
+    RVEncoder* AllocRVEncoderTypeIZeroOperand() noexcept;
+
+
+#define __RV_ENCODER_DECLARATIONS \
+        virtual void        SetImmediate(imm_t imm) noexcept override; \
+        virtual void        SetRD(int rd) noexcept override; \
+        virtual void        SetRS1(int rs1) noexcept override; \
+        virtual void        SetRS2(int rs2) noexcept override; \
+        virtual insnraw_t   Get() const noexcept override;
+
+    // 
+    class RVEncoderTypeR : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeR(int opcode, int funct3, int funct7) noexcept;
+        RVEncoderTypeR(int opcode, int funct3, int funct7, bool rd_required, bool rs1_required, bool rs2_required) noexcept;
+
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeI : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeI(int opcode, int funct3) noexcept;
+        RVEncoderTypeI(int opcode, int funct3, bool imm_required, bool rd_required, bool rs1_required) noexcept;
+
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeS : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeS(int opcode, int funct3) noexcept;
+        RVEncoderTypeS(int opcode, int funct3, bool imm_required, bool rs1_required, bool rs2_required) noexcept;
+
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeB : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeB(int opcode, int funct3) noexcept;
+        RVEncoderTypeB(int opcode, int funct3, bool imm_required, bool rs1_required, bool rs2_required) noexcept;
+
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeU : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeU(int opcode) noexcept;
+        RVEncoderTypeU(int opcode, bool imm_required, bool rd_requried) noexcept;
+
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeJ : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeJ(int opcode) noexcept;
+        RVEncoderTypeJ(int opcode, bool imm_required, bool rd_required) noexcept;
+        
+        __RV_ENCODER_DECLARATIONS
+    };
+
+    class RVEncoderTypeIZeroOperand : public RVEncoder {
+    protected:
+        insnraw_t   insn;
+
+    public:
+        RVEncoderTypeIZeroOperand(int opcode, int funct3, int funct12) noexcept;
+    
+        __RV_ENCODER_DECLARATIONS
+    };
 }
 
 
@@ -295,3 +414,269 @@ namespace Jasse {
     }
 }
 
+
+
+// Implementation of: class RVEncoderTypeR
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeR::RVEncoderTypeR(int opcode, int funct3, int funct7) noexcept
+        : RVEncoder (false, true, true, true)
+        , insn      (ENCODE_RVTYPE_R(opcode, funct3, 0, 0, 0, funct7))
+    { }
+
+    RVEncoderTypeR::RVEncoderTypeR(int opcode, int funct3, int funct7, bool rd_required, bool rs1_required, bool rs2_required) noexcept
+        : RVEncoder (false, rd_required, rs1_required, rs2_required)
+        , insn      (ENCODE_RVTYPE_R(opcode, funct3, 0, 0, 0, funct7))
+    { }
+
+    void RVEncoderTypeR::SetImmediate(imm_t imm) noexcept
+    { }
+
+    void RVEncoderTypeR::SetRD(int rd) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rd, RV_OPERAND_RD);
+    }
+
+    void RVEncoderTypeR::SetRS1(int rs1) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs1, RV_OPERAND_RS1);
+    }
+
+    void RVEncoderTypeR::SetRS2(int rs2) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs2, RV_OPERAND_RS2);
+    }
+
+    insnraw_t RVEncoderTypeR::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeI
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeI::RVEncoderTypeI(int opcode, int funct3) noexcept
+        : RVEncoder (true, true, true, false)
+        , insn      (ENCODE_RVTYPE_I(opcode, funct3, 0, 0, 0))
+    { }
+
+    RVEncoderTypeI::RVEncoderTypeI(int opcode, int funct3, bool imm_required, bool rd_required, bool rs1_required) noexcept
+        : RVEncoder (imm_required, rd_required, rs1_required, false)
+        , insn      (ENCODE_RVTYPE_I(opcode, funct3, 0, 0, 0))
+    { }
+
+    void RVEncoderTypeI::SetImmediate(imm_t imm) noexcept
+    {
+        insn |= ENCODE_IMM_RVTYPE_I(imm);
+    }
+
+    void RVEncoderTypeI::SetRD(int rd) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rd, RV_OPERAND_RD);
+    }
+
+    void RVEncoderTypeI::SetRS1(int rs1) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs1, RV_OPERAND_RS1);
+    }
+
+    void RVEncoderTypeI::SetRS2(int rs2) noexcept
+    { }
+
+    insnraw_t RVEncoderTypeI::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeS
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeS::RVEncoderTypeS(int opcode, int funct3) noexcept
+        : RVEncoder (true, false, true, true)
+        , insn      (ENCODE_RVTYPE_S(opcode, funct3, 0, 0, 0))
+    { }
+
+    RVEncoderTypeS::RVEncoderTypeS(int opcode, int funct3, bool imm_required, bool rs1_required, bool rs2_required) noexcept
+        : RVEncoder (imm_required, false, rs1_required, rs2_required)
+        , insn      (ENCODE_RVTYPE_S(opcode, funct3, 0, 0, 0))
+    { }
+
+    void RVEncoderTypeS::SetImmediate(imm_t imm) noexcept
+    {
+        insn |= ENCODE_IMM_RVTYPE_S(imm);
+    }
+
+    void RVEncoderTypeS::SetRD(int rd) noexcept
+    { }
+
+    void RVEncoderTypeS::SetRS1(int rs1) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs1, RV_OPERAND_RS1);
+    }
+
+    void RVEncoderTypeS::SetRS2(int rs2) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs2, RV_OPERAND_RS2);
+    }
+
+    insnraw_t RVEncoderTypeS::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeB
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeB::RVEncoderTypeB(int opcode, int funct3) noexcept
+        : RVEncoder (true, false, true, true)
+        , insn      (ENCODE_RVTYPE_B(opcode, funct3, 0, 0, 0))
+    { }
+
+    RVEncoderTypeB::RVEncoderTypeB(int opcode, int funct3, bool imm_required, bool rs1_required, bool rs2_required) noexcept
+        : RVEncoder (imm_required, false, rs1_required, rs2_required)
+        , insn      (ENCODE_RVTYPE_B(opcode, funct3, 0, 0, 0))
+    { }
+
+    void RVEncoderTypeB::SetImmediate(imm_t imm) noexcept
+    {
+        insn |= ENCODE_IMM_RVTYPE_B(imm);
+    }
+
+    void RVEncoderTypeB::SetRD(int rd) noexcept
+    { }
+
+    void RVEncoderTypeB::SetRS1(int rs1) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs1, RV_OPERAND_RS1);
+    }
+
+    void RVEncoderTypeB::SetRS2(int rs2) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rs2, RV_OPERAND_RS2);
+    }
+
+    insnraw_t RVEncoderTypeB::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeU
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeU::RVEncoderTypeU(int opcode) noexcept
+        : RVEncoder (true, true, false, false)
+        , insn      (ENCODE_RVTYPE_U(opcode, 0, 0))
+    { }
+
+    RVEncoderTypeU::RVEncoderTypeU(int opcode, bool imm_required, bool rd_required) noexcept
+        : RVEncoder (imm_required, rd_required, false, false)
+        , insn      (ENCODE_RVTYPE_U(opcode, 0, 0))
+    { }
+
+    void RVEncoderTypeU::SetImmediate(imm_t imm) noexcept
+    {
+        insn |= ENCODE_IMM_RVTYPE_U(imm);
+    }
+
+    void RVEncoderTypeU::SetRD(int rd) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rd, RV_OPERAND_RD);
+    }
+
+    void RVEncoderTypeU::SetRS1(int rs1) noexcept
+    { }
+
+    void RVEncoderTypeU::SetRS2(int rs2) noexcept
+    { }
+
+    insnraw_t RVEncoderTypeU::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeJ
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeJ::RVEncoderTypeJ(int opcode) noexcept
+        : RVEncoder (true, true, false, false)
+        , insn      (ENCODE_RVTYPE_J(opcode, 0, 0))
+    { }
+
+    RVEncoderTypeJ::RVEncoderTypeJ(int opcode, bool imm_required, bool rd_required) noexcept
+        : RVEncoder (imm_required, rd_required, false, false)
+        , insn      (ENCODE_RVTYPE_J(opcode, 0, 0))
+    { }
+
+    void RVEncoderTypeJ::SetImmediate(imm_t imm) noexcept
+    {
+        insn |= ENCODE_IMM_RVTYPE_J(imm);
+    }
+
+    void RVEncoderTypeJ::SetRD(int rd) noexcept
+    {
+        insn |= ENCODE_STD_OPERAND(rd, RV_OPERAND_RD);
+    }
+
+    void RVEncoderTypeJ::SetRS1(int rs1) noexcept
+    { }
+
+    void RVEncoderTypeJ::SetRS2(int rs2) noexcept
+    { }
+
+    insnraw_t RVEncoderTypeJ::Get() const noexcept
+    {
+        return insn;
+    }
+}
+
+// Implementation of: class RVEncoderTypeIZeroOperand
+namespace Jasse {
+    /*
+    insnraw_t   insn;
+    */
+
+    RVEncoderTypeIZeroOperand::RVEncoderTypeIZeroOperand(int opcode, int funct3, int funct12) noexcept
+        : RVEncoder (false, false, false, false)
+        , insn      (ENCODE_RVTYPE_I(opcode, funct3, 0, 0, funct12))
+    { }
+
+    void RVEncoderTypeIZeroOperand::SetImmediate(imm_t imm) noexcept
+    { }
+
+    void RVEncoderTypeIZeroOperand::SetRD(int rd) noexcept
+    { }
+
+    void RVEncoderTypeIZeroOperand::SetRS1(int rs1) noexcept
+    { }
+
+    void RVEncoderTypeIZeroOperand::SetRS2(int rs2) noexcept
+    { }
+
+    insnraw_t RVEncoderTypeIZeroOperand::Get() const noexcept
+    {
+        return insn;
+    }
+}
