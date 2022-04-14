271ff8f101356b5946ffad5d23503e74a78f71f2
Kumonda221 <kumonda@kucro3.org>
2022-04-14 23:10:39 +0800

    Commit RISC-V ISA basis components (RVEncoder)

--- main/emulated/isa/riscv.hpp
+++ main/emulated/isa/riscv.hpp
@@ -311,6 +311,41 @@ namespace Jasse {
     };
 
 
+    // RISC-V Instruction Encoder
+    class RVEncoder {
+    private:
+        const bool  imm_required;
+        const bool  rd_required;
+        const bool  rs1_required;
+        const bool  rs2_required;
+
+    public:
+        RVEncoder() noexcept;
+        RVEncoder(bool imm_required, bool rd_required, bool rs1_required, bool rs2_required) noexcept;
+        ~RVEncoder() noexcept;
+
+        virtual void        SetImmediate(imm_t imm) noexcept = 0;
+        virtual void        SetRD(int rd) noexcept = 0;
+        virtual void        SetRS1(int rs1) noexcept = 0;
+        virtual void        SetRS2(int rs2) noexcept = 0;
+
+        virtual insnraw_t   Get() const noexcept = 0;
+
+        bool                IsImmediateRequired() const noexcept;
+        bool                IsRDRequired() const noexcept;
+        bool                IsRS1Required() const noexcept;
+        bool                IsRS2Required() const noexcept;
+
+        RVEncoder&          Imm(imm_t imm) noexcept;
+        RVEncoder&          RD(int rd) noexcept;
+        RVEncoder&          RS1(int rs1) noexcept;
+        RVEncoder&          RS2(int rs2) noexcept;
+    };
+
+    // RISC-V Instruction Encoder Allocator
+    typedef RVEncoder*  (*RVEncoderAllocator)();
+
+
     // RISC-V Instruction Decoder Trait Function
     typedef bool        (*RVDecoderTrait)(insnraw_t insnraw, RVInstruction& insn);
 
@@ -985,7 +1020,7 @@ namespace Jasse {
 
     RVInstruction::RVInstruction()
         : insn          (0)
-        , imm           ({ 0 })
+        , imm           (0)
         , rd            (0)
         , rs1           (0)
         , rs2           (0)
@@ -998,7 +1033,7 @@ namespace Jasse {
         , rd            (obj.rd)
         , rs1           (obj.rs1)
         , rs2           (obj.rs2)
-        , trait         ()
+        , trait         (obj.trait)
     { }
 
     RVInstruction::~RVInstruction()
@@ -1107,6 +1142,71 @@ namespace Jasse {
     inline std::string RVInstruction::ToString() const
     {
         return GetTextualizer()(*this);
+    }
+}
+
+
+// Implementation of: class RVEncoder
+namespace Jasse {
+    RVEncoder::RVEncoder() noexcept
+        : imm_required  (true)
+        , rd_required   (true)
+        , rs1_required  (true)
+        , rs2_required  (true)
+    { }
+
+    RVEncoder::RVEncoder(bool imm_required, bool rd_required, bool rs1_required, bool rs2_required) noexcept
+        : imm_required  (imm_required)
+        , rd_required   (rd_required)
+        , rs1_required  (rs1_required)
+        , rs2_required  (rs2_required)
+    { }
+
+    RVEncoder::~RVEncoder() noexcept
+    { }
+
+    inline RVEncoder& RVEncoder::Imm(imm_t imm) noexcept
+    {
+        SetImmediate(imm);
+        return *this;
+    }
+
+    inline RVEncoder& RVEncoder::RD(int rd) noexcept
+    {
+        SetRD(rd);
+        return *this;
+    }
+
+    inline RVEncoder& RVEncoder::RS1(int rs1) noexcept
+    {
+        SetRS1(rs1);
+        return *this;
+    }
+
+    inline RVEncoder& RVEncoder::RS2(int rs2) noexcept
+    {
+        SetRS2(rs2);
+        return *this;
+    }
+
+    inline bool RVEncoder::IsImmediateRequired() const noexcept
+    {
+        return imm_required;
+    }
+
+    inline bool RVEncoder::IsRDRequired() const noexcept
+    {
+        return rd_required;
+    }
+
+    inline bool RVEncoder::IsRS1Required() const noexcept
+    {
+        return rs1_required;
+    }
+
+    inline bool RVEncoder::IsRS2Required() const noexcept
+    {
+        return rs2_required;
     }
 }
 
@@ -1384,9 +1484,9 @@ namespace Jasse {
         RVMOPStatus mopstatus;
 
         if (arch.XLEN() == XLEN32) // XLEN=32
-            mopstatus = MI->Read(arch.PC().pc32, MOPW_WORD, &fetched);
+            mopstatus = MI->ReadInsn(arch.PC().pc32, MOPW_WORD, &fetched);
         else // XLEN=64
-            mopstatus = MI->Read(arch.PC().pc64, MOPW_WORD, &fetched);
+            mopstatus = MI->ReadInsn(arch.PC().pc64, MOPW_WORD, &fetched);
 
         if (mopstatus != MOP_SUCCESS)
         {
